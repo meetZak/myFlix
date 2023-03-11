6493b2241573ed959b6583b5ecb6e8a346beab4c
abuyusr <abdizm@outlook.com>
2023-03-11 14:04:26 -0500

    Integrating Mongoose

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-// Load express module by using require function and call function app.
-const express = require('express');
-      morgan = require('morgan');
-      app = express();
-      bodyParser = require('body-parser');
-      uuid = require('uuid');
+// Load required modules and call function app.
+  const express = require('express');
+        bodyParser = require('body-parser');
+        uuid = require('uuid');
+  
+  const app = express();
+  const morgan = require('morgan');
+        fs = require('fs');
+        path = require ('path');
 
+// Integrating Mongoose to allow REST API to perform CRUD.
+  const mongoose = require('mongoose');
+  const Models = require ('./models.js');
+const path = require('path');
 
+// Creation of variables to call databases.
+  const Movies = Models.Movie;
+  const Users = Models.User;
+  const Genres = Models.Genre;
+  const Directors = Models.Director;    
+
+// Method allowing Mongoose to connect to that database so it can perform CRUD operations .
+  mongoose.connect ('mongodb://localhost:27017/cfDB',
+ {useNewUrlParser:true,useUnifiedTopology:true});
 
 
 // We need to enable parsing json object. This is adding a piece of middleware.
-app.use (bodyParser.json());
-//app.use(express.static('public'));
+  app.use (bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended:true}));
+  app.use(express.static('public'));
 
 
 //Define an array of users and movies with couple of properties. 
@@ -89,6 +106,11 @@ let movies = [
   },
  
   ]
+
+  // default text response when at/
+  app.get('/', (req, res) => {
+    res.send('Welcome to MyFlix Movie App!');
+  });
 //Handling Http get request to get list of all movies.
 app.get('/movies', (req, res) => {
   res.status(200).json(movies);
@@ -125,19 +147,35 @@ app.get('/movies/director/:directorName', (req, res) => {
     res.status(400).send('no such director!')
   }
 })
-// Handling HTTP Post Requests to allow new users to register.
+//  Post Requests to allow new users to register with Mongoose.
 app.post('/users', (req, res) => {
-  const newUser = req.body;
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send('users need name');
-  }
-})
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
 // Handling HTTP put  Requests to allow users to update their info.
-app.put('/users/:id', (req, res) => {
+ app.put('/users/:id', (req, res) => {
   const { id } = req.params;
   const updateUser = req.body;
   let user = users.find( user => user.id == id );
@@ -147,11 +185,11 @@ app.put('/users/:id', (req, res) => {
   } else {
     res.status(400).send('No such user')
   }
-})
+}) 
 
 // Handling HTTP Post Requests allowing users to  add movie to their list of favorit.
 
-app.post('/users/:id/:movieTitle', (req, res) => {
+ app.post('/users/:id/:movieTitle', (req, res) => {
   const { id, movieTitle } = req.params;
   let user = users.find( user => user.id == id );
   if (user) {
@@ -160,9 +198,9 @@ app.post('/users/:id/:movieTitle', (req, res) => {
   } else {
     res.status(400).send('No such movie')
   }
-})
+}) 
 // Handling HTTP delete  Requests to remove a movies from their favorite.
-app.delete('/users/:id/:movieTitle', (req, res) => {
+ app.delete('/users/:id/:movieTitle', (req, res) => {
   const { id, movieTitle } = req.params;
   let user = users.find( user => user.id == id );
 
@@ -172,9 +210,9 @@ app.delete('/users/:id/:movieTitle', (req, res) => {
   } else {
     res.status(400).send('No such user')
   }
-})
+}) 
 // Handling HTTP delete  Requests allowing users to de-register.
-app.delete('/users/:id', (req, res) => {
+ app.delete('/users/:id', (req, res) => {
   const { id } = req.params;
   let user = users.find( user => user.id == id );
   if (user) {
@@ -183,7 +221,7 @@ app.delete('/users/:id', (req, res) => {
   } else {
     res.status(400).send('No such user')
   }
-})
+}) 
 
 
 // created code that can handle unanticipated errors.
---
+++ models.js
@@ -0,0 +1,39 @@
+
+// Importing  Mongoose package into models.js file.
+const mongoose = require('mongoose');
+
+
+// Defining Schema for the movies collection
+let moviesSchema = mongoose.Schema ({
+    Title: { type:String, required:true},
+    Description:{ type: String , required: true},
+    Genre:{
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
+// Defining Schema for the users collection.
+let userSchema = mongoose.Schema ({
+    Username: { type: String, required:true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref:'Movie'}]
+
+});
+
+//Creating models using the schemas.
+let Movie = mongoose.model ('Movie',moviesSchema);
+let User = mongoose.model ('User',userSchema);
+
+// Exporting the models in order to import in Index.js file.
+module.exports.Movie = Movie;
+module.exports.User = User;
