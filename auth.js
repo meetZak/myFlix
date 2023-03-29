31798c118b1d2dfaf233bdaae59d249b1584a027
abuyusr <abdizm@outlook.com>
2023-03-29 17:47:29 -0400

    updated

--- auth.js
+++ auth.js
@@ -21,7 +21,7 @@ module.exports = (router) => {
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: 'Something is not right',
+          message: ' not right',
           user: user
         });
       }
@@ -30,8 +30,8 @@ module.exports = (router) => {
           res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token });
+        return res.json({user, token});
       });
     })(req, res);
   });
-}
+} 
