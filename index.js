d56e6154f9bfe430f03d4d0e329a9cfc3d93b661
abuyusr <abdizm@outlook.com>
2023-05-12 21:32:04 -0400

    updated index file

--- index.js
+++ index.js
@@ -21,7 +21,8 @@
 
 // Importing auth.js and requiring Passport Module into the project.
     const cors = require('cors');
-    let allowedOrigins = ['http://localhost:8080','https://zaflix.herokuapp.com/','http://localhost:1234','https://myflixmovie-app.netlify.app/login'];
+    app.use(cors());
+   /*  let allowedOrigins = ['http://localhost:8080','https://zaflix.herokuapp.com/','http://localhost:1234','https://myflixmovie-app.netlify.app/login'];
     app.use(cors({
       origin: (origin, callback) => {
         if(!origin) return callback(null, true);
@@ -31,7 +32,7 @@
         }
         return callback(null, true);
       }
-    }));
+    })); */
    let auth = require('./auth')(app);
     const passport = require ('passport');
     require ('./passport');
