fcd12e7f8f8100a72444d5ad85690d654d985d0b
abuyusr <abdizm@outlook.com>
2023-08-28 22:03:20 -0400

    updated

--- index.js
+++ index.js
@@ -1,385 +1,74 @@
-/**
- * root file of the myFlix back end application
- * importing packages required for the project
- * @requires express
- * @requires bodyParser
- * @requires fs
- * @requires path
- * @requires uuid
- * @requires morgan
- * @requires CORS
- * @requires ./auth
- * @requires ./passport
- * @requires mongoose
- * @requires ./models.js
- * @requires express-validator
- */
-const express = require('express'),
-  bodyParser = require('body-parser'),
-  fs = require('fs'),
-  path = require ('path'),
-  uuid = require('uuid'),
-  morgan = require('morgan');
-
-const app = express();  
+// Load required modules and call function app.
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+const uuid = require("uuid");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const Movies = Models.Movie;
+const Users = Models.User;
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const { check, validationResult } = require('express-validator');
+// Importing the express-validator library into file.
+const { check, validationResult } = require ('express-validator');
 
+// Importing auth.js and requiring Passport Module into the project.
 const cors = require('cors');
-app.use(cors());
-
-// keeping below code for if/when reverting back to limited origins access
-/* let allowedOrigins = ['http://localhost:8080','https://zaflix.herokuapp.com/','http://localhost:1234','https://myflixmovie-app.netlify.app/login'];
+/*let allowedOrigins = ['http://localhost:8080','https://zaflix.herokuapp.com/','http://localhost:1234','https://myflixmovie-app.netlify.app/login'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
       return callback(new Error(message ), false);
     }
     return callback(null, true);
   }
 })); */
-
 let auth = require('./auth')(app);
-const passport = require('passport');
-require('./passport');
-
-const mongoose = require('mongoose');
-const Models = require('./models.js');
-
-const Movies = Models.Movie;
-const Users = Models.User;
-const Genres = Models.Genre;
-const Directors = Models.Director;
-
-// connection to middleware
-accessLogStream = fs.createWriteStream(path.join(__dirname, './log.txt.log'), {flags: 'a'});
-
-// keeping below code for local connetion use
-/* mongoose.connect('mongodb://127.0.0.1:27017/myflixDB', { useNewUrlParser: true, useUnifiedTopology: true }); */
-
-/**
- * connection to online database hosted by mongoDB
- */
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+const passport = require ('passport');
+require ('./passport');
+//Integrating Mongoose with RESTAPI cfDB is the name od Database with movies and users
+/*   mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });*/
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+ 
+app.use(morgan('common', {stream: accessLogStream}));
 app.use(express.static('public'));
-
-// creates a txt log file, recording any requests made to the API
-app.use(morgan('common', {stream: fs.createWriteStream('./log.txt.log', {flags: 'a'})}));
-app.use(morgan('dev'));
-
-/**
- * Welcome page text response
- * @function
- * @method GET - endpoint '/'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - HTTP response object with the welcome message
- */
+// default text response when at/
 app.get('/', (req, res) => {
-  res.send('Welcome to MyFlix Movie App!');
+res.send('Welcome to MyFlix Movie App!');
 });
-
-/**
- * Retrieves a list of all movies
- * @function
- * @method GET - endpoint '/movies'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - HTTP response object with the list of movies
- */
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Movies.find()
-    .then((movies) => {
-      res.json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
-/**
- * Retrieves a specific movie by its' title
- * @function
- * @method GET - endpoint '/movies/:Title'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - HTTP response object with the info of one movie
- */
-app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Movies.findOne({ Title: req.params.Title })
-    .then((movie) => {
-      res.status(201).json(movie);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
-/**
- * Retrieves a specific Genre by its' title
- * @function
- * @method GET - endpoint '/movies/genre/:Name'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - HTTP response object with info of one genre
- */
-app.get('/movies/genre/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Movies.findOne({ 'Genre.Name': req.params.Name })
-    .then((movies) => {
-      res.send(movies.Genre);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
-/**
- * Retrieves a specific Director by their Name
- * @function
- * @method GET - endpoint '/movies/director/:directorName'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - HTTP response object with info of one director
- */
-app.get('/movies/director/:directorName', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Movies.findOne({ 'Director.Name': req.params.directorName })
-    .then((movies) => {
-      res.send(movies.Director);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
-/**
- * Adds a new movie to the database by filling out required information
- * @function
- * @method POST - endpoint '/movies'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - JSON object holding data about a movie
- */
-app.post('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-  const movie = await Movies.findOne({Title: req.body.Title})
-  if (movie) {
-    return res.status(400).send(req.body.Title + 'already exists');
-  } else {
-    const newMovie = await Movies.create({
-      Title: req.body.Title,
-      Description: req.body.Description,
-      Genre: {
-        Name: req.body.Genre.Name,
-        Description: req.body.Genre.Description
-      },
-      Director: {
-        Name: req.body.Director.Name,
-        Bio: req.body.Director.Bio,
-        Birthyear: req.body.Director.Birthyear,
-        Deathyear: req.body.Director.Deathyear
-      },
-      ImagePath: req.body.ImagePath,
-      Featured: req.body.Featured,
-      Release: req.body.Release
-    })
-      res.status(201).json(newMovie)
-  }
-} catch (error) {
-  console.error(error);
-  res.status(500).send('Error' + error);
-}
-});
-
-/**
- * Retreives all registered users
- * @function
- * @method GET - endpoint '/users'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - HTTP response object with a list of users 
- */
-app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.status(201).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
-/**
- * Retrieves a specific user by their Name
- * @function
- * @method GET - endpoint '/users/:Username'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - JSON object holding data of the user
- */
-app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOne({ Username: req.params.Username })
-    .then((users) => {
-      res.status(201).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
-/**
- * Allows users to register by filling out required information
- * @function
- * @method POST - endpoint '/users'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - JSON object holding data of the new user
- */
-app.post('/users',
-  [
-    check('Username', 'Username is required and minimum length is 5 characters').isLength({min: 5}),
-    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-    check('Password', 'Password is required').not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isEmail(),
-    // check('Birthday', 'Birthday should be in the format DD/MM/YYYY').isDate({format:'DD/MM/YYYY'})
-  ], (req, res) => {
-    let errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
-
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({ Username: req.body.Username })
-      .then((user) => {
-        if (user) {
-          return res.status(400).send(req.body.Username + 'already exists');
-        } else {
-          Users.create({
-          // req.body is data input by user, the keys (eg Email) correlate to a field in models.js 
-            Username: req.body.Username,
-            Password: hashedPassword,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-            .then((user) => {
-              res.status(201).json(user) 
-            })
-            .catch((error) => {
-              console.error(error);
-              res.status(500).send('Error: ' + error);
-            });
-          }
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
-      });
-    });
-
-/**
- * Allows existing user to update their information
- * @function
- * @method PATCH - endpoint '/users/:Username'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {object} - JSON object holding updated data of the user
- */
-app.patch('/users/:Username', passport.authenticate('jwt', { session: false }), 
- (req, res) => {
-  let errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
-
-  let updateObj = {};
-  if (req.body.Username !== undefined) updateObj.Username = req.body.Username;
-  if (req.body.Password !== undefined) updateObj.Password = Users.hashPassword(req.body.Password);
-  if (req.body.Email !== undefined) updateObj.Email = req.body.Email;
-  if (req.body.Birthday !== undefined) updateObj.Birthday = req.body.Birthday;
-
-  Users.findOneAndUpdate({ Username: req.params.Username }, { $set: updateObj },
-  { new: true }, // This line makes sure that the updated document is returned
-  (err, updatedUser) => {
-    if(err) {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    } else {
-      res.json(updatedUser);
-    }
+//Handling  get request to get list of all movies with Mongoose.
+app.get('/movies',passport.authenticate('jwt',{session:false}),(req, res) => {
+Movies.find()
+  .then((movies) => {
+    res.json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
   });
 });
-
-  /**
-   * Allows registered users to add a movie to their favorites
-   * @function
-   * @method POST - endpoint '/users/:Username/movies/:MovieID'
-   * @param {object} - HTTP request object
-   * @param {object} - HTTP response object
-   * @returns {object} - JSON object holding updated data of the user
-   */
-  app.post('/users/:Username/movies/:MovieID', passport.authenticate( 'jwt', { session: false }), (req, res) => {
-    Users.findOneAndUpdate({ Username: req.params.Username }, 
-       { $push: { FavoriteMovies: req.params.MovieID }
-     },
-     { new: true }, // This line makes sure that the updated document is returned
-     (err, updatedUser) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      } else {
-        res.json(updatedUser);
-      }
-    });
+//Handling Http get request to get specific  movie by title with Mongoose.
+app.get('/movies/:title', passport.authenticate('jwt',{session:false}),(req, res) => {
+Movies.findOne({ Title: req.params.title })
+  .then((movie) => {
+    res.status(201).json(movie);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
   });
-
-  /**
-  * Allows registered users to remove a movie from their favorites
-  * @function
-  * @method DELETE - endpoint '/users/:Username/movies/:MovieID'
-  * @param {object} - HTTP request object
-  * @param {object} - HTTP response object
-  * @returns {object} - JSON object holding updated data of the user
-  */
-  app.delete('/users/:Username/movies/:MovieID', passport.authenticate( 'jwt', { session: false }), (req, res) => {
-    Users.findOneAndUpdate({ Username: req.params.Username }, 
-       { $pull: { FavoriteMovies: req.params.MovieID }
-      },
-      { new: true },
-      function(err, updatedUser) {  
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error: " + err);
-        } else {
-          res.json(updatedUser);
-        }
-      });
-  });
-
-/**
- * Allows a registered user to delete their account
- * @function
- * @method DELETE - endpoint '/users/:Username'
- * @param {object} - HTTP request object
- * @param {object} - HTTP response object
- * @returns {string} - message confirming account deletion
- */
-app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOneAndRemove({ Username: req.params.Username })
-  .then((user) => {
-    if (!user) {
-      res.status(400).send(req.params.Username + ' was not found');
-    } else {
-      res.status(200).send(req.params.Username + ' was deleted.');
-    }
+});
+//Handling  get request to get specific movie by genre with Mongoose.
+app.get('/movies/genre/:Name',passport.authenticate('jwt', { session: false }), (req, res) => {
+Movies.findOne({ 'Genre.Name': req.params.genreName })
+  .then((movie) => {
+    res.status(201).json(movie.Genre);
   })
   .catch((err) => {
     console.error(err);
@@ -387,14 +76,132 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
   });
 });
 
-// error handler middleware function, place after all route calls
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('It\'s not working right now!');
+//Handling  get request to get specific movie by director with Mongoose.
+app.get('/movies/director/:directorName',passport.authenticate('jwt', { session: false }), (req, res) => {
+Movies.findOne({ 'Director.Name': req.params.directorName })
+  .then((movie) => {
+    res.status(201).json(movie.Director);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
   });
+});
+// Handling Get request for all users with Mongoose.
+app.get('/users',passport.authenticate('jwt',{session:false}), (req, res) => {
+Users.find()
+.then((users) => {
+  res.status(201).json(users);
+})
+.catch((err) => {
+  console.error(err);
+  res.status(500).send('Error: ' + err);
+});
+});
+// Handling Get request for a specific user by username with Mongoose.
+app.get('/users/:Username',passport.authenticate('jwt', { session: false }), (req, res) => {
+Users.findOne({ Username: req.params.Username })
+.then((users) => {
+res.status(201).json(users);
+})
+.catch((err) => {
+console.error(err);
+res.status(500).send('Error: ' + err);
+});
+});
+//  Post Requests to allow new users to register with Mongoose.
+app.post('/users',
+[
+check('Username', 'Username is required').isLength({min: 5}),
+check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+check('Password', 'Password is required').not().isEmpty(),
+check('Email', 'Email does not appear to be valid').isEmail()
+], (req, res) => {
+// check the validation object for errors
+let errors = validationResult(req);
+if (!errors.isEmpty()) {
+return res.status(422).json({ errors: errors.array() });
+}
+let hashedPassword = Users.hashPassword(req.body.Password);
+Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+.then((user) => {
+  if (user) {
+    //If the user is found, send a response that it already exists
+    return res.status(400).send(req.body.Username + ' already exists');
+  } else {
+    Users
+      .create({
+        Username: req.body.Username,
+        Password: hashedPassword,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then((user) => { res.status(201).json(user) })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  }
+})
+.catch((error) => {
+  console.error(error);
+  res.status(500).send('Error: ' + error);
+});
+});
+// Update a user's info, by username
+app.put('/users/:Username',passport.authenticate('jwt', { session: false }), (req, res) => {
+ Users.findOneAndUpdate({ Username: req.params.Username }, req.body, { new: true })
+.then(updatedUser => {
+res.status(200).json(updatedUser);
+})
+.catch(error => {
+res.status(500).json({ error: error.message });
+});
+}
+);
+ 
+/* POST: allow users to add a movie to their favourites with MONGOOSE  */
+app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }),(req, res) => {
+Users.findOneAndUpdate({ Username: req.params.Username }, {
+ $push: { FavoriteMovies: req.params.MovieID }
+},
+{ new: true }, // This line makes sure that the updated document is returned
+(err, updatedUser) => {
+if (err) {
+  console.error(err);
+  res.status(500).send('Error: ' + err);
+} else {
+  res.json(updatedUser);
+}
+});
+});
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+Users.findOneAndRemove({ Username: req.params.Username })
+.then((user) => {
+  if (!user) {
+    res.status(400).send(req.params.Username + ' was not found');
+  } else {
+    res.status(200).send(req.params.Username + ' was deleted.');
+  }
+})
+.catch((err) => {
+  console.error(err);
+  res.status(500).send('Error: ' + err);
+});
+});
+// Access documentation.html using express.static.
+app.get('/documentation', (req, res) => {                  
+res.sendFile('public/documentation.html', { root: __dirname });
+});
+// created code that can handle unanticipated errors.
+app.use((err, req, res, next) => {
+console.error(err.stack);
+res.status(500).send('Something broke!');
+}); 
 
-// starts server on specified port and listens for requests 
+// listen on port.
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+console.log('Listening on Port ' + port);
 });
--- models.js
+++ models.js
@@ -1,58 +1,45 @@
+// Importing  Mongoose package into models.js file.
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require ('bcrypt');
 
-// defines movieSchema format to be used in mongoDB 
-let movieSchema = mongoose.Schema({
-    Title: {type: String, required: true},
-    Description: {type: String, required: true},
-    Genre: {
+
+// Defining Schema for the movies collection
+let movieSchema = mongoose.Schema ({
+    Title: { type:String, required:true},
+    Description:{ type: String , required: true},
+    Genre:{
         Name: String,
         Description: String
     },
     Director: {
         Name: String,
-        Bio: String,
-        Birthyear: Date,
-        Deathyear: Date
+        Bio: String
     },
-    Release: Date,
+    Actors: [String],
     ImagePath: String,
-    Featured: Boolean 
+    Featured: Boolean
 });
-
-// defines userSchema format to be used in mongoDB
-let userSchema = mongoose.Schema({
-    Username: { type: String, required: true },
-    Password: { type: String, required: true },
-    Email: { type: String, required: true },
+// Defining Schema for the users collection.
+let userSchema = mongoose.Schema ({
+    Username: { type: String, required:true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
     Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref:'Movie'}]
+
 });
 
-/**
- * converts user password into hashed format using bcrypt.js
- * @function hashPassword
- * @param {string} password as input by user
- * @returns {string} hashed @password of user input
- */
-userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
+userSchema.statics.hashPassword = (password)=>{
+    return bcrypt.hashSync (password, 10);
 };
 
-/**
- * validates user password by comparing it with the stored hashed password
- * @function validatePassword
- * @param {string} password to be validated
- * @returns {string} validated @password
- */
-userSchema.methods.validatePassword = function(password) {
+userSchema.methods.validatePassword = function (password) {
     return bcrypt.compareSync(password, this.Password);
 };
-  
-// creates models based on defined Schemas
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
 
-// export defined modules so they can be used
+//Creating models using the schemas.
+let Movie = mongoose.model ('Movie',movieSchema);
+let User = mongoose.model ('User',userSchema);
+// Exporting the models in order to import in Index.js file.
 module.exports.Movie = Movie;
 module.exports.User = User;
