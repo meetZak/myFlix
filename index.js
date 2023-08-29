7c0f3c076b03f46752df4d3b35f3ad35639b96c7
abuyusr <abdizm@outlook.com>
2023-08-29 12:24:59 -0400

    updated

--- index.js
+++ index.js
@@ -16,21 +16,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const { check, validationResult } = require ('express-validator');
 
 // Importing auth.js and requiring Passport Module into the project.
-const cors = require('cors');
-/*let allowedOrigins = ['http://localhost:8080','https://zaflix.herokuapp.com/','http://localhost:1234','https://myflixmovie-app.netlify.app/login'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-})); */
-let auth = require('./auth')(app);
-const passport = require ('passport');
-require ('./passport');
+    const cors = require('cors');
+    app.use(cors());
+  /*   let allowedOrigins = ['http://localhost:8080','https://zaflix.herokuapp.com/','http://localhost:1234','https://myflixmovie-app.netlify.app/login'];
+    app.use(cors({
+      origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+          let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+          return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+      }
+    }));  */
+   let auth = require('./auth')(app);
+    const passport = require ('passport');
+    require ('./passport');
 //Integrating Mongoose with RESTAPI cfDB is the name od Database with movies and users
 /*   mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });*/
 mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
